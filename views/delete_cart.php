efeae1dc42b081177358983d1f2798ba8db74909
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-07-28 14:36:52 +0700

    delete

--- views/delete_cart.php
+++ views/delete_cart.php
@@ -1,9 +1,9 @@
 <?php
 // Nạp file session để sử dụng phiên đăng nhập
 include_once '../lib/session.php';
+
 // Kiểm tra xem người dùng đã đăng nhập với quyền "client" chưa
 Session::checkSession('client');
+
 // Nạp model giỏ hàng để sử dụng các hàm liên quan đến giỏ hàng
 include_once '../models/cart.php';
-
-?>

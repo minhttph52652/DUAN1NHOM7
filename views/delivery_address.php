6950f8e1632d8b2c1a65f4a628eb4551164b54e6
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-07-29 16:32:21 +0700

    Footer

--- views/delivery_address.php
+++ views/delivery_address.php
@@ -106,5 +106,20 @@ $result = $order->getOrderByUser();
         </div>
     </div>
 </div>
+
+<!-- Phần chân trang -->
+<footer>
+    <div class="social">
+        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    </div>
+    <ul class="list">
+        <li><a href="./">Trang Chủ</a></li>
+        <li><a href="productList.php">Sản Phẩm</a></li>
+    </ul>
+    <p class="copyright">copy by IVYmoda.com 2024</p>
+</footer>
+
 </body>
 </html>

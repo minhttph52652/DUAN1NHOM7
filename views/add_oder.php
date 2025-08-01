ba8fc5fa4b8a4d90601bc2aec55150b391a1a0ab
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-08-01 15:11:04 +0700

    ktra nếu request dc gửi = POST

--- views/add_oder.php
+++ views/add_oder.php
@@ -7,3 +7,12 @@ Session::checkSession('client');
 
 // Gọi model order để xử lý các chức năng liên quan đến đơn hàng
 include_once __DIR__ . '/../models/order.php';
+
+// Kiểm tra nếu request được gửi bằng phương thức POST (tức là khi người dùng gửi form đặt hàng)
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $order = new order();           // Tạo một đối tượng từ class order
+    $result = $order->add($_POST);  // Gọi phương thức add() và truyền dữ liệu form để tạo đơn hàng mới
+
+   
+}
+?>

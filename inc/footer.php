eece2f9afe456bcd394cf97c6e74cac677a48280
minhttph52652 <minhttph52652@gmail.com>
2025-08-01 23:07:18 +0700

    footer

---
+++ inc/footer.php
@@ -0,0 +1,29 @@
+</div>
+    <div class ="footer">
+        <div class="wrapper">
+               <div class="section group">
+                   <div class="col_1_of_4 span_1_of_4 ">
+                       <h4> Liên hệ chúng tôi</h4>
+                         <ul>
+                              <li> <span>ĐT:0342722918</span></li>
+                               <li> <span>Email: minhttph52652@gmail.com</span></li>
+                                <li> <span>Địa chỉ: Mai Dịch, Cầu Giấy , Hà Nội</span></li>
+                         </ul>
+                         <div class="social-incons">
+                              <h4>Theo dõi chúng tôi</h4>
+                                <ul>
+                                    <li class="facebook"><a href="#" target="_blank"></a></li>
+                                    <li class="twitter"><a href="#" target="_blank"></a></li>
+                                    <li class="googleplus"><a href="#" target="_blank"></a></li>
+                                    <li class="contact"><a href="#" target="_blank"></a></li>
+                                    <div class="clear"></div>
+                                </ul>
+                         </div>
+                   </div>
+               </div>
+                 <div class="copy_right">
+                     <p> 2025 IVYmoda.com</p>
+        </div>
+
+    </div>
+     </div>

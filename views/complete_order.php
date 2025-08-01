f69ecbaba5f41b1204268f708554e30400586689
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-08-01 15:22:29 +0700

    ktra URL

--- views/complete_order.php
+++ views/complete_order.php
@@ -7,3 +7,16 @@ Session::checkSession('client');
 
 // Nạp model xử lý đơn hàng
 include_once '../models/order.php';
+
+// Kiểm tra xem trên URL có truyền tham số orderId (ID đơn hàng) hay không
+if (isset($_GET['orderId'])) {
+
+    // Tạo đối tượng order
+    $order = new order();
+
+    // Gọi hàm completeOrder để đánh dấu đơn hàng là đã hoàn tất
+    $result = $order->completeOrder($_GET['orderId']);
+
+
+
+}

dfd645b9f9aba577f33f62dc4d6824fa28d15d8e
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-08-04 14:43:24 +0700

    Footer

--- view/edit_info.php
+++ view/edit_info.php
@@ -145,5 +145,18 @@ $totalQty = $cart->getTotalQtyByUserId();
         </div>
     </div>
 
+    <!-- Footer -->
+    <footer>
+        <div class="social">
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        <ul class="list">
+            <li><a href="./">Trang Chủ</a></li>
+            <li><a href="productList.php">Sản Phẩm</a></li>
+        </ul>
+        <p class="copyright">copy by IVYmoda.com 2024</p>
+    </footer>
 
 </body>

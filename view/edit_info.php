3e7631a567a1c9b4b76c8c28cc974ce7399ea220
Le Xuan Thuy <thuytg17112005@gmail.com>
2025-08-04 14:32:09 +0700

    html

--- view/edit_info.php
+++ view/edit_info.php
@@ -23,3 +23,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     }
 }
 ?>
+
+<?php
+// Gồm các file session và models
+include_once '../lib/session.php';
+include_once '../models/product.php';
+include_once '../models/cart.php';
+
+// Tạo đối tượng cart và lấy tổng số sản phẩm
+$cart = new cart();
+$totalQty = $cart->getTotalQtyByUserId();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://use.fontawesome.com/2145adbb48.js"></script>
+    <script src="https://kit.fontawesome.com/a42aeb5b72.js" crossorigin="anonymous"></script>
+    <title>Cập nhật thông tin cá nhân</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script>
+        // Tự động chuyển ảnh trong banner
+        $(function() {
+            $('.fadein img:gt(0)').hide();
+            setInterval(function() {
+                $('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');
+            }, 5000);
+        });
+    </script>
+</head>
